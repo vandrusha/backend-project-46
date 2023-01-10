c0f71a5fe8b8f7c89578828352a763d02697c2a9
vandrusha <a.a.metelsky@gmail.com>
2023-01-10 20:22:15 +0100

    modified gendiff and package

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,9 @@
-const { Command } = require('commander');
+import { Command } from 'commander';
 const program = new Command();
 program
     .name('gendiff')
-    .description('CLI to some JavaScript string utilities')
-    .version('1.0.0');
-program.command('gendiff')
-    .description('Compares 1 two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number')
-    .option('-h, --help', 'display help for command');
+    .option('-h, --help', 'display help for command')
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
 program.parse();
-export default program;
+if (program.opts().h || program.opts().help) program.help();
