95a067b04cac9ce65d848d0adb89f805fffefc9f
vandrusha <a.a.metelsky@gmail.com>
2023-01-11 20:17:56 +0100

    removed redundant comment

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,8 +3,13 @@ import { Command } from 'commander';
 const program = new Command();
 program
     .name('gendiff')
+    .version('1.0.0')
     .option('-h, --help', 'display help for command')
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
+    .arguments('<filepath1> <filepath2>')
+    .action((filepath1, filepath2) => {
+        console.log('filepath1:', filepath1);
+        console.log('filepath2:', filepath2);
+    })
+    .option('-f, --format <type>', 'output format')
 program.parse();
-if (program.opts().h || program.opts().help) program.help();
