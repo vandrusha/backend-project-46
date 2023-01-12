84222acefa799acc4b3eb093a4a06a9162872bb9
vandrusha <a.a.metelsky@gmail.com>
2023-01-12 21:20:14 +0100

    added support of yaml files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-import genDiff from '@hexlet/code';
-
-const diff = genDiff(filepath1, filepath2);
-console.log(diff);
+import _ from 'lodash';
+const comparator = (file1, file2) => {
+    let results = [];
+    _.forIn(file1, (value, key) => {
+        if (_.has(file2, key) && file2[key] === value) {
+            results.push([' ', key, value]);
+        } else if (_.has(file2, key)) {
+            results.push(['-', key, value]);
+            results.push(['+', key, file2[key]]);
+        } else {
+            results.push(['-', key, value]);
+        }
+    });
+    _.forIn(file2, (value, key) => {
+        if (!_.has(file1, key)) {
+            results.push(['+', key, value]);
+        }
+    })
+    const sortedResults = _.sortBy(results, a => a[1]);
+    const diffStr = sortedResults
+        .map(item => `${item[0]} ${item[1]}: ${item[2]}`)
+        .join('\n');
+    console.log(`{\n${diffStr}\n}`);
+};
+export default comparator;
