a812a7f657c947e5775fd38ac4ec0471aea0a1d2
vandrusha <a.a.metelsky@gmail.com>
2023-01-12 14:41:44 +0100

    added libs to

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import comparator from '../src/index.js';
+import path from 'node:path';
+import { readFileSync } from 'node:fs';
+
 const program = new Command();
 program
     .name('gendiff')
@@ -8,8 +12,13 @@ program
     .description('Compares two configuration files and shows a difference.')
     .arguments('<filepath1> <filepath2>')
     .action((filepath1, filepath2) => {
-        console.log('filepath1:', filepath1);
-        console.log('filepath2:', filepath2);
+        const resolvedPath1 = path.resolve(filepath1);
+        const resolvedPath2 = path.resolve(filepath2);
+        const file1 = readFileSync(resolvedPath1);
+        const file2 = readFileSync(resolvedPath2);
+        const fileObj1 = JSON.parse(file1);
+        const fileObj2 = JSON.parse(file2);
+        comparator(fileObj1, fileObj2);
     })
     .option('-f, --format <type>', 'output format')
 program.parse();
