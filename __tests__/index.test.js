f87da619f87f41d39ded2e5a0232054adfbed52a
vandrusha <a.a.metelsky@gmail.com>
2023-01-20 09:23:05 +0100

    added tests for yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,6 +3,7 @@ import comparator from '../src/index.js';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import YAML from 'yaml';
 //import { beforeAll } from 'node:test';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,15 +11,22 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-let file1;
-let file2;
+let jsonFile1;
+let jsonFile2
+let yamlFile1;
+let yamlFile2;
 let expectedResult;
 
 beforeAll(() => {
-    file1 = JSON.parse(readFile('file1.json'));
-    file2 = JSON.parse(readFile('file2.json'));
+    jsonFile1 = JSON.parse(readFile('file1.json'));
+    jsonFile2 = JSON.parse(readFile('file2.json'));
+    yamlFile1 = YAML.parse(readFile('file1.yaml'));
+    yamlFile2 = YAML.parse(readFile('file2.yaml'));
     expectedResult = String(readFile('expectedResult.txt'));
 });
-test('comparator', () => {
-    expect(comparator(file1, file2)).toEqual(expectedResult);
+test('json files', () => {
+    expect(comparator(jsonFile1, jsonFile2)).toEqual(expectedResult);
+});
+test('yaml files', () => {
+    expect(comparator(yamlFile1, yamlFile2)).toEqual(expectedResult);
 });
