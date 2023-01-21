f612ffa0a982e76a71604174a899874ce0d470b6
vandrusha <a.a.metelsky@gmail.com>
2023-01-21 10:09:14 +0100

    changed domain model and adjusted stylish

--- src/formatters.js
+++ src/formatters.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 const ind = (num) => {
     return ' '.repeat(num);
 }
+const sortObj = (obj) => Object.keys(obj).sort((a, b) => a > b ? 1 : -1).reduce((a, b) => { a[b] = obj[b]; return a }, {});
 
 const objToStr = (obj, mul) => {
     let result = '';
@@ -16,24 +17,44 @@ const objToStr = (obj, mul) => {
     return `${result}`;
 };
 const stylish = (file) => {
-    const iter = (arr, mul) => {
-        const sortedArr = _.sortBy(arr, a => a[1]);
-        let diffStr = sortedArr
-            .map(item => {
-                let result = '';
-                if (_.isArray(item[2])) {
-                    result = `${ind(mul)}${item[0]} ${item[1]}: {\n${iter(item[2], mul + 4)}\n${ind(mul + 2)}}`;
-                } else if (_.isObject(item[2])) {
-                    result = `${ind(mul)}${item[0]} ${item[1]}: {${objToStr(item[2], mul + 4)}\n${ind(mul + 2)}}`;
+    const iter = (fileObj, mul) => {
+        const sortedObj = sortObj(fileObj);
+        let result = '';
+        const stateMap = {
+            equal: ' ',
+            removed: '-',
+            updated: '-',
+            added: '+'
+        };
+        _.forIn(sortedObj, (value, key) => {
+            const pattern = `${ind(mul)}${stateMap[value.state]} ${key}: `;
+            if (value.state === 'equal') {
+                if (_.values(value.children).length !== 0) {
+                    result += `${pattern}{\n${iter(value.children, mul + 4)}${ind(mul + 2)}}`;
                 } else {
-                    result = `${ind(mul)}${item[0]} ${item[1]}: ${item[2]}`;
+                    result += `${pattern}${value.origValue}`;
                 }
-                return result;
-            })
-            .join('\n');
-        return diffStr;
+            } else if (value.state === 'removed' || value.state === 'added') {
+                if (_.isObject(value.origValue)) {
+                    result += `${pattern}{${objToStr(value.origValue, mul + 4)}\n${ind(mul + 2)}}`
+                } else {
+                    result += `${pattern}${value.origValue}`
+                }
+            } else {
+                result += _.isObject(value.origValue)
+                    ? `${ind(mul)}- ${key}: {${objToStr(value.origValue, mul + 4)}\n${ind(mul + 2)}}\n`
+                    : `${ind(mul)}- ${key}: ${value.origValue}\n`
+                result += _.isObject(value.newValue)
+                    ? `${ind(mul)}+ ${key}: {${objToStr(value.newValue, mul + 4)}\n${ind(mul + 2)}}`
+                    : `${ind(mul)}+ ${key}: ${value.newValue}`
+            }
+            result += '\n';
+        });
+        return result;
     }
-    return `{\n${iter(file, 2)}\n}`;
+    return `{\n${iter(file, 2)}}`;
+};
+const plain = (file) => {
 };
 
-export { stylish };
+export { stylish, plain };
--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 import _ from 'lodash';
 import * as formatter from './formatters.js'
-
+const makeCompObj = (state, origValue = '', newValue = '', children = {}) => ({ state, origValue, newValue, children });
 const comparator = (obj1, obj2, style = 'stylish') => {
     const diff = (file1, file2) => {
-        const results = [];
+        const results = {};
         _.forIn(file1, (value, key) => {
             if (!_.isObject(value)) {
                 if (_.has(file2, key) && file2[key] === value) {
-                    results.push([' ', key, value]);
+                    results[key] = makeCompObj('equal', value);
                 } else if (_.has(file2, key)) {
-                    results.push(['-', key, value]);
-                    results.push(['+', key, file2[key]]);
+                    results[key] = makeCompObj('updated', value, file2[key]);
                 } else {
-                    results.push(['-', key, value]);
+                    results[key] = makeCompObj('removed', value);
                 }
             } else if (!_.has(file2, key)) {
-                results.push(['-', key, value]);
+                results[key] = makeCompObj('removed', value);
             } else if (!_.isObject(file2[key])) {
-                results.push(['-', key, value]);
-                results.push(['+', key, file2[key]]);
+                results[key] = makeCompObj('updated', value, file2[key]);
             } else {
-                results.push([' ', key, diff(value, file2[key])]);
+                results[key] = makeCompObj('equal', '', '', diff(value, file2[key]));
             }
         });
         _.forIn(file2, (value, key) => {
             if (!_.has(file1, key)) {
-                results.push(['+', key, value]);
+                results[key] = makeCompObj('added', value);
             }
         });
         return results;
@@ -34,28 +32,3 @@ const comparator = (obj1, obj2, style = 'stylish') => {
     return formatter[style](diffArr);
 };
 export default comparator;
-
-
-    // let results = [];
-    // _.forIn(file1, (value, key) => {
-    //     if (_.has(file2, key) && file2[key] === value) {
-    //         results.push([' ', key, value]);
-    //     } else if (_.has(file2, key)) {
-    //         results.push(['-', key, value]);
-    //         results.push(['+', key, file2[key]]);
-    //     } else {
-    //         results.push(['-', key, value]);
-    //     }
-    // });
-    // _.forIn(file2, (value, key) => {
-    //     if (!_.has(file1, key)) {
-    //         results.push(['+', key, value]);
-    //     }
-    // })
-    // const sortedResults = _.sortBy(results, a => a[1]);
-
-    // let diffStr = sortedResults
-    //     .map(item => `${item[0]} ${item[1]}: ${item[2]}`)
-    //     .join('\n');
-    // diffStr = `{\n${diffStr}\n}`;
-    // return diffStr;
