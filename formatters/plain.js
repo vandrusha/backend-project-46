33345bc9de47a3f605113199db6c1c93f02c5536
vandrusha <a.a.metelsky@gmail.com>
2023-01-28 10:04:40 +0100

    fix plain logic

--- formatters/plain.js
+++ formatters/plain.js
@@ -27,8 +27,10 @@ const plain = (file, root = '') => {
       result.push(`Property '${root}${key}' was removed`);
     } else if (value.state === 'added') {
       result.push(`Property '${root}${key}' was added with value: ${parsedValue(value.origValue)}`);
-    } else if (value.state === 'equal' && _.values(value.children).length !== 0) {
-      result.push(plain(value.children, `${root}${key}.`));
+    } else if (value.state === 'equal') {
+        if (_.values(value.children).length !== 0) {
+          result.push(plain(value.children, `${root}${key}.`));
+        }
     } else {
       if (_.isObject(value.origValue) && _.isObject(value.newValue)) {
         result.push(`Property '${root}${key}' was updated. From [complex value] to [complex value]`);
