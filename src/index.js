e5179fc418edb68190a93c34f55af3035eb03f7d
vandrusha <a.a.metelsky@gmail.com>
2023-01-29 21:31:58 +0100

    remove mutation from file parser

--- src/index.js
+++ src/index.js
@@ -16,32 +16,29 @@ const comparator = (filepath1, filepath2, style = 'stylish') => {
   const fileExtension2 = path.extname(resolvedPath2);
   const obj1 = parser(parsedFile1, fileExtension1);
   const obj2 = parser(parsedFile2, fileExtension2);
-  const diff = (file1, file2) => {
-    const results = {};
-    _.forIn(file1, (value, key) => {
-      if (!_.isObject(value)) {
-        if (_.has(file2, key) && file2[key] === value) {
-          results[key] = makeCompObj('equal', value);
-        } else if (_.has(file2, key)) {
-          results[key] = makeCompObj('updated', value, file2[key]);
-        } else {
-          results[key] = makeCompObj('removed', value);
-        }
-      } else if (!_.has(file2, key)) {
-        results[key] = makeCompObj('removed', value);
-      } else if (!_.isObject(file2[key])) {
-        results[key] = makeCompObj('updated', value, file2[key]);
+  const diff = (file1, file2) => Object.entries(file1).reduce((acc, entry) => {
+    const key = entry[0];
+    const value = entry[1];
+    if (!_.isObject(value)) {
+      if (file2[key] === value) {
+        acc[key] = makeCompObj('equal', value);
+      } else if (_.has(file2, key)) {
+        acc[key] = makeCompObj('updated', value, file2[key]);
       } else {
-        results[key] = makeCompObj('equal', '', '', diff(value, file2[key]));
+        acc[key] = makeCompObj('removed', value);
       }
-    });
-    _.forIn(file2, (value, key) => {
-      if (!_.has(file1, key)) {
-        results[key] = makeCompObj('added', value);
-      }
-    });
-    return results;
-  };
+    } else if (!_.has(file2, key)) {
+      acc[key] = makeCompObj('removed', value);
+    } else if (!_.isObject(file2[key])) {
+      acc[key] = makeCompObj('updated', value, file2[key]);
+    } else {
+      acc[key] = makeCompObj('equal', '', '', diff(value, file2[key]));
+    }
+    Object.entries(file2)
+      .filter(entry2 => !_.has(file1, entry2[0]))
+      .map(entry2 => acc[entry2[0]] = makeCompObj('added', entry2[1]));
+    return acc;
+  }, {});
   const diffArr = diff(obj1, obj2);
   return formatter(diffArr, style);
 };
