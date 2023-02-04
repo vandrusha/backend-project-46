c88f040fc465b2f7a3724caeb73b18571f745f51
vandrusha <a.a.metelsky@gmail.com>
2023-02-04 11:40:56 +0100

    remove mutation from tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,25 +9,14 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
 
-let jsonFile1;
-let jsonFile2;
-let yamlFile1;
-let yamlFile2;
-let expectedResult;
-let expectedPlain;
-let expectedJson;
-
-beforeAll(() => {
-  jsonFile1 = getFixturePath('file1.json');
-  jsonFile2 = getFixturePath('file2.json');
-  yamlFile1 = getFixturePath('file1.yaml');
-  yamlFile2 = getFixturePath('file2.yaml');
-  expectedResult = String(readFile('expectedResult.txt'));
-  expectedPlain = String(readFile('expectedPlain.txt'));
-  expectedJson = String(readFile('expectedJson.txt'));
-});
+const jsonFile1 = getFixturePath('file1.json');
+const jsonFile2 = getFixturePath('file2.json');
+const yamlFile1 = getFixturePath('file1.yaml');
+const yamlFile2 = getFixturePath('file2.yaml');
+const expectedResult = String(readFile('expectedResult.txt'));
+const expectedPlain = String(readFile('expectedPlain.txt'));
+const expectedJson = String(readFile('expectedJson.txt'));
 test('json files, default format', () => {
-
   expect(comparator(jsonFile1, jsonFile2)).toEqual(expectedResult);
 });
 test('yaml files, default format', () => {
